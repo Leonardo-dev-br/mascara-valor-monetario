5cb136836388c2a339f4d50b5afc663d026c8cbd
Leonardo-dev-br <99846550+Leonardo-dev-br@users.noreply.github.com>
2023-05-09 15:44:57 -0300

    Alterações e adição de Listener

--- funcao.js
+++ funcao.js
@@ -1,12 +1,26 @@
 
-function formatarValor(){
-    var valorInputado = document.getElementById("numform").value //Pegando valor Inputado pelo User
-    console.log(valorInputado.indexOf(',')) //Mostra a posição da virgula no Valor Inputado pelo User
+var valorInputado = document.getElementById("numform").value //Pegando valor Inputado pelo User
+    valorInputado.addEventListener(valorInputado, formatarValor())
+    
+    function formatarValor(){
 
-    //Autocomplemento para valor Inputado ,00
-    if(valorInputado === 1){
-        valorInputado.value += '00'   
+    //console.log(valorInputado.indexOf(',')) //Mostra a posição da virgula no Valor Inputado pelo User
+
+    // Verifica se o valor é numérico
+    if(!isNaN(valorInputado)) {
+        var formattedNumber = parseFloat(valorInputado).toFixed(2).replace('.', ',');
+        input.valorInputado = formattedNumber;
+    }
+
+   //Transforma o valor que vc recebeu do usuário em float
+parseFloat(valorInputado)
+
+//Deixa duas casas depois da vírgula 
+.toFixed(2)
+
+//Troca o . default do folat, pela vírgula que vc quer 
+.replace('.', ',');   
    } 
-}
 
-           
+
+        
